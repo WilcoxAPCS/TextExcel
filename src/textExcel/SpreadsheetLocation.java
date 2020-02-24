3dad46b9a1cdcde2a28f44f22967e7b1bed90e6f
Steven Okai <sokai@outlook.com>
2020-02-24 09:07:49 -0800

    Update comments for SpreadsheetLocation

--- src/textExcel/SpreadsheetLocation.java
+++ src/textExcel/SpreadsheetLocation.java
@@ -4,6 +4,13 @@ package textExcel;
 
 public class SpreadsheetLocation implements Location
 {
+    /* Examples:
+     *  | cellName  | getRow() returns |
+     *  --------------------------------
+     *  | "A1"      | 0                |
+     *  | "B1"      | 0                |
+     *  | "A2"      | 1                |
+     */
     @Override
     public int getRow()
     {
@@ -11,6 +18,13 @@ public class SpreadsheetLocation implements Location
         return 0;
     }
 
+    /* Examples:
+     *  | cellName  | getCol() returns |
+     *  --------------------------------
+     *  | "A1"      | 0                |
+     *  | "B1"      | 1                |
+     *  | "A2"      | 0                |
+     */
     @Override
     public int getCol()
     {
@@ -18,6 +32,7 @@ public class SpreadsheetLocation implements Location
         return 0;
     }
     
+    /* Takes a String cellName (example: "A1") as a parameter. */
     public SpreadsheetLocation(String cellName)
     {
         // TODO: Fill this out with your own code
